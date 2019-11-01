96e0c912d7269d9371ecb5143ec7f68c5f5b7e39
Tathagata Basu <txqg68@perelman.dur.ac.uk>
2019-11-01 10:24:59 +0000

    updated

--- R/optim.R
+++ R/optim.R
@@ -7,8 +7,6 @@
 #
 ###########################################################################
 
-
-
 #' Optimisation tool for non smooth functions
 #'
 #' Function for minimising piecewise differentiable objective functions.
@@ -16,129 +14,130 @@
 #' @param theta Initial value.
 #' @param fn Function to optimise.
 #' @param df Subgradient of the objective function.
-#' @param method Method to be used for optimisation. Default is subgradient
-#' method.
 #' @param tol Relative convergence tolerance
-#' @param maxit Maximum number of iterations if relative convergence is not
-#' reached. Default is 1000.
-#' @param h Stepsize for updating the set of parameters. Default is 0.001.
-#' @param prox Proximal operator for proximal gradient method. Default is
-#' NULL.
-#' @param acl Flag for accelerated proximal gradient method. Default is false.
-#' @param theta_it Iterative theta for coordinate descent algorithm. Default
-#' is NULL.
+#' @param t Stepsizes for updating the set of parameters.
 #' @return The function returns a list with following components
 #' \item{par}{The best set of parameters}
 #' \item{value}{The value of the function corresponding to best set of parameters.}
 #' \item{iteration}{Number of iterations taken}
-#' \item{method}{The method used for the optimisation}
 #' @export
 
-nonsmoothOptim = function(theta, fn, df, method = "SG", tol = 1e-5, maxit = 1000, h = 0.001,
-                        prox = NULL, acl = FALSE, theta_it = NULL)
-{
-  if(length(theta) != length(df(theta)))
-    stop("Number of parameters does not match with length of the sub gradient vector")
-
-  if(is.null(prox) & any(method == c("PG", "pg"))){
-    warning("No proximal operator provided, the method defaults to sub gradient method")
-    method = "SG"
-  }
-
-  if(is.null(theta_it) && any(method == c("CD", "cd"))){
-    warning("No iterative theta provided, the method defaults to sub gradient method")
-    method = "SG"
-  }
-
-  if(!any(method == c("SG", "sg", "PG", "pg", "CD", "cd"))){
-    warning("Wrong input for method, the method defaults to sub gradient method")
-    method = "SG"
-  }
-
-  # Sub-gradient optimisation.
-  sg = function(theta, fn, df, tol, maxit, h) {
-    value = fn(theta)
-    par = theta.last = theta
-    t = c(rep(h, maxit %/% 2), h / (1:(maxit - maxit %/% 2)))
-    for (i in 1:maxit) {
-      theta = theta.last - t[i] * df(theta.last)
-      if(is.na(sum((df(theta) ^ 2))))
-        stop("the subgradient vector is returning undefined value, change stepsize")
-      fx = fn(theta)
-      if(fx < value) {
-        par = theta
-        value = fx
-      }
-      if(sqrt(sum((df(theta) ^ 2))) < tol)
-        break()
-      else
-        theta.last = theta
+sgOptim = function(theta, fn, df, tol = 1e-6, t) {
+  value = fn(theta)
+  par = theta.last = theta
+  maxit = length(t)
+  for (i in 1:maxit) {
+    theta = theta.last - t[i] * df(theta.last)
+    if(is.na(sum((df(theta) ^ 2))))
+      stop("the subgradient vector is returning undefined value, change stepsize")
+    fx = fn(theta)
+    if(fx < value) {
+      par = theta
+      value = fx
     }
-
-    output = list("par" = as.vector(par), "value" = value, "iteration" = i, "method" = "sub gradient")
-    output
-  }
-
-  # Proximal gradient optimisation.
-
-  pg = function(theta, fn, df, prox, acl, tol, maxit, h) {
-    par = theta.prev = theta
-    t = c(rep(h, maxit %/% 2), h / (1:(maxit - maxit %/% 2)))
-    if(isTRUE(acl)){
-      for (i in 1:maxit) {
-        v = theta + (i - 2) * (theta - theta.prev) / (i + 1)
-        par = prox(t[i], v - t[i] * df(v))
-        theta.prev = theta
-        theta = par
-        if(is.na(sqrt(sum((df(theta) ^ 2)))))
-          stop("the subgradient vector is returning undefined value, change stepsize")
-        if(sqrt(sum((df(theta) ^ 2))) < tol)
-          break()
-      }
-      throw = "accelerated proximal gradient"
-    }
-    else{
-      for (i in 1:maxit) {
-        par = prox(t[i], theta - t[i] * df(theta))
-        theta = par
-        if(is.na(sqrt(sum((df(theta) ^ 2)))))
-          stop("the subgradient vector is returning undefined value, change stepsize")
-        if(sqrt(sum((df(theta) ^ 2))) < tol)
-          break()
-      }
-      throw = "proximal gradient"
-    }
-    value = fn(par)
-
-    output = list("par" = as.vector(par), "value" = value, "iteration" = i, "method" = throw)
-    output
-  }
-
-  # Co-ordinate descent optimisation.
-
-  cd = function(theta, fn, theta_it, tol, maxit) {
-    for (j in 1:maxit) {
+    if(sqrt(sum((df(theta) ^ 2))) < tol)
+      break()
+    else
       theta.last = theta
-      for(i in 1:length(theta)) {
-        theta[i] = theta_it(theta, i)
-      }
-      if(sqrt(sum(((theta.last - theta) ^ 2))) < tol)
-        break
-    }
-    value = fn(theta)
-
-    output = list("par" = theta, "value" = value, "iteration" = j, "method" = "coordinate descent")
-    output
   }
 
-  if(any(method == c("SG", "sg")))
-    output = sg(theta = theta, fn = fn, df = df, tol = tol, maxit = maxit, h = h)
-  if(any(method == c("PG", "pg")))
-    output = pg(theta = theta, fn = fn, df = df, prox = prox, acl = acl,
-                tol = tol, maxit = maxit, h = h)
-  if(any(method == c("CD", "cd")))
-    output = cd(theta = theta, fn = fn, theta_it = theta_it, tol = tol, maxit = maxit)
-
-
+  output = list("par" = as.vector(par), "value" = value, "iteration" = i)
   output
 }
+
+#' Proximal gradient optimisation for non smooth functions
+#'
+#' Function for minimising piecewise differentiable objective functions.
+#'
+#' @param theta Initial value.
+#' @param fn Function to optimise.
+#' @param df Subgradient of the objective function.
+#' @param prox Proximal operator for proximal gradient method.
+#' @param acl Flag for accelerated proximal gradient method. Default is false.
+#' @param tol Relative convergence tolerance
+#' @param t Stepsizes for updating the set of parameters.
+#' @return The function returns a list with following components
+#' \item{par}{The best set of parameters}
+#' \item{value}{The value of the function corresponding to best set of parameters.}
+#' \item{iteration}{Number of iterations taken}
+#' @export
+
+pgOptim = function(theta, fn, df, prox, acl = F, tol = 1e-6, t) {
+  par = theta.prev = theta
+  maxit = length(t)
+  if(acl){
+    for (i in 1:maxit) {
+      v = theta + (i - 2) * (theta - theta.prev) / (i + 1)
+      par = prox(t[i], v - t[i] * df(v))
+      theta.prev = theta
+      theta = par
+      if(is.na(sqrt(sum((df(theta) ^ 2)))))
+        stop("the subgradient vector is returning undefined value, change stepsize")
+      if(sqrt(sum((df(theta) ^ 2))) < tol)
+        break()
+    }
+  }
+  else{
+    for (i in 1:maxit) {
+      par = prox(t[i], theta - t[i] * df(theta))
+      theta = par
+      if(is.na(sqrt(sum((df(theta) ^ 2)))))
+        stop("the subgradient vector is returning undefined value, change stepsize")
+      if(sqrt(sum((df(theta) ^ 2))) < tol)
+        break()
+    }
+  }
+  value = fn(par)
+
+  output = list("par" = as.vector(par), "value" = value, "iteration" = i)
+  output
+}
+
+#' Coordinate descent optimisation for non smooth functions
+#'
+#' Function for minimising piecewise differentiable objective functions.
+#'
+#' @param theta Initial value.
+#' @param fn Function to optimise.
+#' @param theta_it Iterative theta for coordinate descent algorithm. Default
+#' is NULL.
+#' @param tol Relative convergence tolerance
+#' @param maxit Maximum number of iterations if relative convergence is not
+#' reached. Default is 1000.
+#' @return The function returns a list with following components
+#' \item{par}{The best set of parameters}
+#' \item{value}{The value of the function corresponding to best set of parameters.}
+#' \item{iteration}{Number of iterations taken}
+#' @export
+
+cdOptim = function(theta, fn, theta_it, tol = 1e-6, maxit = 1000) {
+  for (j in 1:maxit) {
+    theta.last = theta
+    for(i in 1:length(theta)) {
+      theta[i] = theta_it(theta, i)
+    }
+    if(sqrt(sum(((theta.last - theta) ^ 2))) < tol)
+      break
+  }
+  value = fn(theta)
+
+  output = list("par" = theta, "value" = value, "iteration" = j)
+  output
+}
+
+
+#' Sequence of step sizes for optimization.
+#'
+#' Function to generate stepsize for sub-gradient optimization and proximal-gradient optimization
+#' @param h Starting value. Default is 0.01
+#' @param m Number of constant steps. Default is 1000
+#' @param n Number of diminishing steps. Default is 1000
+#' @return The sequence of stepsize
+#' @export
+
+stepsize = function(h = 0.01, m = 1000, n = 1000)
+  if (n != 0) {
+    c(rep(h, m), h / (1:n))
+  } else {
+    rep(h, m)
+  }
---
+++ R/package.R
@@ -0,0 +1,15 @@
+#' nonsmoothOptim: This package is intended for optimisation
+#' with piecewise differentiable function.
+#'
+#' This is an optimisation package for piecewise differentiable
+#' functions. The package comprise of three different optimisation
+#' techiniques: Subgradient, Proximal gradient and Coordinate
+#' descent. There is also a helper function to generate stepsizes
+#' for subgradient and proximal gradient optimisation.
+#'
+#' @section nonsmoothOptim functions:
+#' Use ls("package:nonsmoothOptim") to check the available functions.
+#'
+#' @docType package
+#' @name nonsmoothOptim
+NULL
